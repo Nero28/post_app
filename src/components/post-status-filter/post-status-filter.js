3b4d6c3cb5e6e7f10153c60d419ac08d7eecaee5
Nero28 <shackiy.s@yandex.ru>
2020-06-15 13:31:04 +0300

    first commit

---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default class PostStatusFilter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filter: '',
+        };
+        this.buttons = [
+            { name: 'all', label: 'Все' },
+            { name: 'like', label: 'Понравилось' },
+        ]
+    }
+
+
+    render() {
+        const buttons = this.buttons.map(({ name, label }) => {
+            const {filter,onFilterSelect}=this.props;
+            const active = filter === name;
+            const clazz = active ? 'btn-info' : 'btn-outline-secondary';
+
+            return (
+                <button
+                    key={name}
+                    type="button"
+                    className={`btn ${clazz}`}
+                    onClick={() => onFilterSelect(name)}
+                >
+                    {label}
+                </button>
+            )
+        });
+
+
+
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        )
+    };
+};
